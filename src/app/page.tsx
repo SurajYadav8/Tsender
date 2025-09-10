23838db28d0105ad243a23b7fba1bbaeeba7f374
SurajYadav8 <raosahab1e@gmail.com>
2025-09-10 15:31:03 +0000

    walletconnect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import { ConnectButton } from "@rainbow-me/rainbowkit"; 
 
 export default function Home() {
   return (
     <div>
-      Hi there!
+      <ConnectButton />
     </div>
   );
 }
