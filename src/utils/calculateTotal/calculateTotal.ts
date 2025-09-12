76f9ea235fb781434e8c988abaab73ac70754723
SurajYadav8 <raosahab1e@gmail.com>
2025-09-12 08:42:23 +0000

    UPDATE

--- src/utils/calculateTotal/calculateTotal.ts
+++ src/utils/calculateTotal/calculateTotal.ts
@@ -1,12 +1,11 @@
-function calculateTotal(amounts: string): number {
+export function calculateTotal(amounts: string): number {
     const amountArray = amounts
-       .split(/[\n,]+/)
-       .map(amt => amt.trim())
-       .filter(amt => amt !== '')
-       .map(amt => parseFloat);
-
-    // Sum all valid amount
-    return amountArray
-        .filter(num => !isNaN(num))
-        .reduce((sum, num) => sum + num, 0)
+        .split(/[,\n]+/)
+        .map(amt => amt.trim())
+        .filter(amt => amt !== "")
+        .map(amt => parseFloat(amt))
+    if (amountArray.some(isNaN)) {
+        return 0
+    }
+    return amountArray.reduce((acc, curr) => acc + curr, 0)
 }
