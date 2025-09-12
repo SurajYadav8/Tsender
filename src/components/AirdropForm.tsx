93160370419a809bf27df74f4e15acd793c5bc3a
SurajYadav8 <raosahab1e@gmail.com>
2025-09-12 08:17:12 +0000

    read contracts

--- src/components/AirdropForm.tsx
+++ src/components/AirdropForm.tsx
@@ -2,14 +2,18 @@
 import {InputForm} from "@/components/ui/InputField"
 import {useState} from "react"
 import {chainsToTSender, tsenderAbi, erc20Abi } from "@/constants"
-import {useChainId} from "wagmi"
-import { log } from "node:console"
+import {useChainId, useAccount} from "wagmi"
+import {readContract} from "@wagmi/core"
+import config from "../rainbowKitConfig"
+
+
 
 export default function AirdropForm() {
     const [tokenAddress, setTokenAddress] = useState("")
     const [recipients, setRecipients] = useState("")
     const [amounts, setAmount] = useState("")
     const chainId = useChainId()
+    const account = useAccount()
 
     async function getApprovedAmount(tsenderAddress: string | null) : Promise<number> {
         if(!tsenderAddress) {
@@ -17,6 +21,15 @@ export default function AirdropForm() {
             return 0
         }
 
+        //read from the chain to see if we have approved enough token
+        const response = await  readContract(config, {
+            abi: erc20Abi,
+            address: tokenAddress as `0x${string}`,
+            functionName: "allowance",
+            args: [account.address, tsenderAddress as `0x${string}`],
+        })
+
+        return response as number 
         
     }
 
@@ -25,6 +38,8 @@ export default function AirdropForm() {
     //    2 Call the airdrop function on the tsender contract 
     //    3 Wait for the transaction to be mined 
         const tsenderAddress = chainsToTSender[chainId]["tsender"]
+        const approvedAmount = await getApprovedAmount(tsenderAddress)
+        
         
     }
 
@@ -41,7 +56,7 @@ export default function AirdropForm() {
             label="Recipients"
             placeholder="0x12345,0x123478"
             value= {recipients}
-            onChange={e => setTokenAddress(e.target.value)} 
+            onChange={e => setRecipients(e.target.value)} 
             large = {true}
             />
 
@@ -49,11 +64,11 @@ export default function AirdropForm() {
             label="Amount"
             placeholder="100, 200, 300"
             value= {amounts}
-            onChange={e => setTokenAddress(e.target.value)} 
+            onChange={e => setAmount(e.target.value)} 
             large = {true}
             />
 
-            <button onClick={handleSubmit}>
+            <button onClick={handleSubmit} className="px-5 py-3 bg-blue-600 hover:bg-blue-700 text -white font-semibold rounded-lg shadow-sm transition-colors duration-200 focus:outline-none focus:ring-2 focus: ring-blue-500 focus:ring-offset-2 disabled:opacity-50 disabled: cursor-not-allowed">
                 Send Tokens
             </button>
         </div>
--- src/rainbowKitConfig.tsx
+++ src/rainbowKitConfig.tsx
@@ -1,11 +1,14 @@
-" use client"
+"use client"
 
-import {getDefaultConfig } from  "@rainbow-me/rainbowkit"
-import { anvil, zksync, mainnet} from "wagmi/chains"
+import { getDefaultConfig } from "@rainbow-me/rainbowkit"
+import { anvil, zksync, mainnet } from "wagmi/chains"
+import { type Config } from "wagmi"
 
-export default getDefaultConfig({
-    appName: "TSender",
-    projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-    chains: [anvil, zksync, mainnet],
-    ssr: false, // this is used for server side rendering
+const config: Config = getDefaultConfig({
+  appName: "TSender",
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+  chains: [anvil, zksync, mainnet],
+  ssr: false,
 })
+
+export default config
